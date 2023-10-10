6afda9c7bb285a8888b1045f37b05be1984ba450
mohamed diop <mouh09diope@gmail.com.com>
2023-10-10 14:50:15 +0200

    save

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = ({heading, message}) => {
           <Link href="/protectform">
         Non
         </Link></button>
-        <button className='px-8 py-2 border mx-2'>Je suis témoin</button>
+        <button className='px-8 py-2 border mx-2'>Je suis témoin </button>
        
       </div>
     </div>
