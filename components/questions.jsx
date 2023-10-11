d23ef856d130c7ee100499657436e2780e10bd61
mohamed diop <mouh09diope@gmail.com.com>
2023-10-11 11:49:38 +0200

    redirection vers le 17

--- components/questions.jsx
+++ components/questions.jsx
@@ -21,6 +21,11 @@ const Danger = ({ heading, message }) => {
 };
 
 export const Call = ({ heading, message }) => {
+  const handleButtonClick = () => {
+    // Redirect to the external website
+    window.location.href = 'https://www.masecurite.interieur.gouv.fr/fr/fiches-pratiques/famille-et-aides-aux-victimes/17-police-secours';
+  };
+
   return (
     <div className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img">
       {/* Overlay */}
@@ -28,13 +33,14 @@ export const Call = ({ heading, message }) => {
       <div className="p-5 text-white z-[2] mt-[-10rem]">
         <h2 className="text-5xl font-bold">{heading}</h2>
         <p className="py-5 text-xl">{message}</p>
-        <button className="px-8 mx-2 py-2 border hover:bg-slate-400">
-          <Link href="/call">Passer un appel</Link>
+        <button className="px-8 mx-2 py-2 border hover:bg-slate-400" onClick={handleButtonClick}>
+          Passer un appel
         </button>
       </div>
     </div>
   );
 };
+
 
 export const AgressionNature = ({ heading, message }) => {
   return (
