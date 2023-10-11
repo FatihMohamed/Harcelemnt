4375791eba38e5fcbc4b3fbe9a1e31bf78ad03d8
mohamed diop <mouh09diope@gmail.com.com>
2023-10-11 21:34:58 +0200

    Scroll effect added

--- components/HeroConTact.jsx
+++ components/HeroConTact.jsx
@@ -1,23 +1,25 @@
 import React from "react";
-import Link from "next/link";
 
-const Hero = ({ heading, message }) => {
+import { Link as ScrollLink } from 'react-scroll';
+
+
+const HeroContact = ({ heading, message }) => {
   return (
     <div className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img">
-      {/* Overlay */}
       <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]" />
       <div className="p-5 text-white z-[2] mt-[-10rem]">
         <h2 className="text-5xl font-bold">{heading}</h2>
         <p className="py-5 text-xl">{message}</p>
-        <button className="px-8 mx-2 py-2 border hover:bg-slate-400">
-          <Link href="/protectform">Oui</Link>
-        </button>
-        <button className="px-8 py-2 border mx-2 hover:bg-slate-400">
-          <Link href="/protectform">Je suis témoin</Link>
-        </button>
+        <ScrollLink to="contactGrid" smooth={true} duration={500}>
+          <button className="px-8 mx-2 py-2 border hover:bg-slate-400">
+            {/* Arrow down Unicode character */}
+            <span className="text-3xl">&#8595;</span>
+          </button>
+        </ScrollLink>
       </div>
     </div>
   );
 };
 
-export default Hero;
+export default HeroContact;
+
