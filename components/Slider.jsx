98175c9eff28cc9eb62d41422f593b59cbd7d1bb
mohamed diop <mouh09diope@gmail.com.com>
2023-10-11 10:25:42 +0200

    sync changes

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -4,19 +4,7 @@ import { SliderData } from './SliderData';
 import { FaArrowCircleLeft, FaArrowCircleRight } from 'react-icons/fa';
 
 const Slider = ({ slides }) => {
-  const [current, setCurrent] = useState(0);
-  const length = slides.length;
 
-  const nextSlide = () => {
-    setCurrent(current === length - 1 ? 0 : current + 1);
-  };
-  const prevSlide = () => {
-    setCurrent(current === 0 ? length - 1 : current - 1);
-  };
-
-  if (!Array.isArray(slides) || slides.length <= 0) {
-    return null;
-  }
 
   return (
     <div id='gallery' className='max-w-[1240px] mx-auto'>
