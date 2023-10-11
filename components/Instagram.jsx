a68f78d24a612906b77397aeb7ceaac4da58a123
mohamed diop <mouh09diope@gmail.com.com>
2023-10-11 14:23:53 +0200

    added links for navigation

--- components/Instagram.jsx
+++ components/Instagram.jsx
@@ -7,22 +7,20 @@ import IgImg5 from '../public/ig-img-5.jpeg';
 import IgImg6 from '../public/ig-img-6.jpeg';
 import InstagramImg from './InstagramImg';
 
-
 const Instagram = () => {
   return (
-    <div className='max-w-[1240px] mx-auto text-center py-24'>
-        <p className='text-2xl font-bold'>Follow me on Instagram</p>
-        <p className='pb-4'>@Captur</p>
-        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-2 p-4'>
-            <InstagramImg socialImg={IgImg1} />
-            <InstagramImg socialImg={IgImg2} />
-            <InstagramImg socialImg={IgImg3} />
-            <InstagramImg socialImg={IgImg4} />
-            <InstagramImg socialImg={IgImg5} />
-            <InstagramImg socialImg={IgImg6} />
-        </div>
+    <div id="instagram" className="max-w-[1240px] mx-auto text-center py-24">
+      <p className="text-2xl font-bold">Témoignages anonymes </p>
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-2 p-4">
+        <InstagramImg socialImg={IgImg1} />
+        <InstagramImg socialImg={IgImg2} />
+        <InstagramImg socialImg={IgImg3} />
+        <InstagramImg socialImg={IgImg4} />
+        <InstagramImg socialImg={IgImg5} />
+        <InstagramImg socialImg={IgImg6} />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Instagram
+export default Instagram;
--- components/questions.jsx
+++ components/questions.jsx
@@ -33,14 +33,16 @@ export const Call = ({ heading, message }) => {
       <div className="p-5 text-white z-[2] mt-[-10rem]">
         <h2 className="text-5xl font-bold">{heading}</h2>
         <p className="py-5 text-xl">{message}</p>
-        <button className="px-8 mx-2 py-2 border hover:bg-slate-400" onClick={handleButtonClick}>
+        <button
+          className="px-8 mx-2 py-2 border hover:bg-slate-400"
+          onClick={handleButtonClick}
+        >
           Passer un appel
         </button>
       </div>
     </div>
   );
 };
-
 
 export const AgressionNature = ({ heading, message }) => {
   return (
@@ -66,5 +68,6 @@ export const AgressionNature = ({ heading, message }) => {
     </div>
   );
 };
+
 
 export default Danger;
