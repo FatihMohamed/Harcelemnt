a6d167ba1e874de2265d4363daed92f5c65df001
mohamed diop <mouh09diope@gmail.com.com>
2023-10-12 11:45:38 +0200

    page d'envoie de mail ajouter

---
+++ pages/assoForm.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {AssoForm} from "../components/Contact";
+
+function assoForm() {
+  return (
+    <div className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img2">
+      <h2 className="text-5xl font-bold"></h2>
+
+      <div>
+        <AssoForm/> 
+      </div>
+    </div>
+  );
+}
+export default assoForm;
