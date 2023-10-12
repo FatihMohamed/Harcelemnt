1ac0ae3d6e8e483623862f4088ef83095df5a2f5
Vehementz <vale-tudo91@sfr.fr>
2023-10-12 11:40:25 +0200

    some progress

--- pages/form.jsx
+++ pages/form.jsx
@@ -4,7 +4,7 @@ import MultiStepForm from '../components/MultiStepForm'
 function form() {
   return (
     <div>
-        <h1 className='mt-24 w-full text-center mx-[auto] mb-12 text-lg'>Form page</h1>
+        <h1 className='mt-24 w-full text-center mx-[auto] mb-12 text-lg'>Formulaire</h1>
             <MultiStepForm />
     </div>
   )
