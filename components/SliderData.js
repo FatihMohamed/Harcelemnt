6a5c4ce5ca1a862470e3e61220f5753c5df9ff90
mohamed diop <mouh09diope@gmail.com.com>
2023-10-12 18:21:02 +0200

    Slider's images changed

--- components/SliderData.js
+++ components/SliderData.js
@@ -1,23 +1,18 @@
+// import IgImg1 from "../public/im1.jpg";
 export const SliderData = [
-    {
-      image:
-        'https://images.unsplash.com/photo-1552035509-b247fe8e5078?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
-    },
-    {
-      image:
-        'https://images.unsplash.com/photo-1560073742-43ed2dc152aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
-    },
-    {
-      image:
-        'https://images.unsplash.com/photo-1560073743-0a45c01b68c4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80',
-    },
-    {
-      image:
-        'https://images.unsplash.com/photo-1528667819581-ca7e29e772bc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80',
-    },
-    {
-      image:
-        'https://plus.unsplash.com/premium_photo-1663089135500-ae1ce586780e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
-    },
-  ];
-  
+  {
+    image: "/im2.png",
+  },
+  {
+    image: "/im3.png",
+  },
+  {
+    image: "/ig-img-1.jpeg",
+  },
+  {
+    image: "/ig-img-2.jpeg",
+  },
+  {
+    image: "/ig-img-3.jpeg",
+  },
+];
